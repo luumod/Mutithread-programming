0406a454d0c3aeb497ab041884eec6afce6b91aa
ylh <1173012900@qq.com>
2023-07-12 19:19:48 +0800

    add 线程管理

---
+++ 3. 线程管理/3. 线程管理/源.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <ctime>
+#include <thread>
+#include <chrono> //时间 
+#include <iomanip>
+
+//获取线程Id
+void threadID() {
+	std::cout << "Id: " << std::this_thread::get_id() << '\n';
+}
+
+//sleep_for延迟函数
+void testSleep_for() {
+	std::cout << "子线程sleep_for: \n";
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+	std::cout << "子线程sleep_for结束: \n";
+}
+
+//yeild
+void testYield(int id) {
+	for (int i = 1; i <= 10000; i++) {
+		//std::cout << "id: " <<id << ": " << i << '\n';
+		std::this_thread::yield();
+	}
+}
+
+void testSleep_until() {
+	//sleep_until： 阻塞线程直到某个时间点
+#if 0 
+	time_t t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	tm* ptm = new tm;
+	localtime_s(ptm, &t);
+	std::cout << std::put_time(ptm, "%X") << '\n';
+	if (ptm != nullptr) {
+		std::this_thread::sleep_until(std::chrono::system_clock::from_time_t(mktime(ptm)));
+		}
+	std::cout << std::put_time(ptm, "%X") << '\n';
+#else 1
+	std::cout << "子线程Sleep_untild: \n";
+	for (int i = 1; i <= 10; i++) {
+		auto startTime = std::chrono::steady_clock::now();
+		std::this_thread::sleep_until(startTime+std::chrono::duration<long long>(2));
+		std::cout << "i= " << i << '\n';
+	}
+	std::cout << "子线程Sleep_untild结束: \n";
+#endif
+
+}
+int main() {
+	std::thread t1(threadID);
+	t1.join();	
+	std::thread t2(testSleep_for);
+	t2.join();
+	std::thread t4(testSleep_until);
+	t4.join();
+
+	std::thread t5(testYield, 1);
+	std::thread t6(testYield, 2);
+	t5.join();
+	t6.join();
+
+	auto start = std::chrono::system_clock::now();
+	for (int i = 1; i <= 100000000; i++) {
+		int m = i + 10;
+		m += 100 * 2 / 2;
+	}
+	auto end = std::chrono::system_clock::now();
+	std::chrono::duration<double, std::milli> tm = end - start;
+	std::cout << "time: " << tm.count() << "ms" << '\n';
+
+	//主线程
+	std::cout << "Id: " << std::this_thread::get_id() << '\n';
+	return 0;
+}
---
+++ 4. 处理线程返回值/4. 处理线程返回值/源.cpp
@@ -0,0 +1,106 @@
+﻿#include <iostream>
+#include <chrono>
+#include <future> 
+using namespace std::this_thread;
+
+int value1(int num) {
+	return num; 
+}
+
+/*
+1. 使用async创建线程处理函数
+2. future对象的.get获取
+*/
+
+//类对象
+class Foo {
+public:
+	Foo() {}
+	int getValue(int num) {
+		std::chrono::milliseconds duration(2000);
+		std::this_thread::sleep_for(duration);
+		return num * 2;
+	}
+};
+
+
+void testAsync() {
+	// launch::async:		创建线程，执行线程处理函数（默认）
+	// launch::deferred：	当调用wait或者get的时候才会创建并执行子线程
+	std::future<int> res1 = std::async(value1, 100);
+	std::cout << res1.get() << '\n';
+
+	Foo m{};
+	std::future<int> res2 = std::async(&Foo::getValue, &m, 200);
+	std::cout << res2.get() << '\n';
+
+	auto res3 = std::async(std::launch::deferred, &Foo::getValue, m, 400);
+	//调用get，执行线程
+	std::cout << res3.get() << '\n';
+}
+
+/*
+1. 通过packaged_task来包装线程处理函数
+2. 使用thread加入线程，执行线程处理函数
+3. 调用 .get_future() 来获取future对象，接着调用 .get() 获取返回值
+*/
+void testPackaged_task() {
+	//1. 普通函数的包装
+	std::packaged_task<int(int)> pack1(value1);
+	std::thread t1(std::ref(pack1),100);  //转换为&&
+	t1.join();
+	std::cout << pack1.get_future().get() << '\n';
+
+	//2. 类中成员函数的包装
+	Foo m{};
+	std::packaged_task<int(int)> pack2(std::bind(&Foo::getValue, &m, std::placeholders::_1));
+	std::thread t2(std::ref(pack2), 200);
+	t2.join();
+	std::cout << pack2.get_future().get() << '\n';
+	
+	//3. lambda表达式
+	std::packaged_task<int(int)> pack3([](int num) {
+		std::cout << "id: " << std::this_thread::get_id() << '\n';
+		return num * 2;
+	});
+	std::thread t3(std::ref(pack3),300);
+	t3.join();
+	std::cout << pack3.get_future().get() << '\n';
+}
+
+/*
+1. 创建Promise对象，使用其set_value来设置返回值
+2. 对这个函数Promise对象的函数创建线程，并且执行
+3. .get_future()获取future，然后.get()获取
+*/
+void testThread(std::promise<int>& pms, int num) {
+	std::cout << get_id() << '\n';
+	pms.set_value(num * 3);
+}
+void testGetValueThread(std::promise<int>& pms) {
+	std::cout << "获取值: " << pms.get_future().get() << '\n';
+}
+void testGetValueThread(int num) {
+	std::cout << "获取值: " << num << '\n';
+}
+void testPromise() {	
+	std::promise<int> pms;
+	std::thread t1(testThread, std::ref(pms), 100);
+	t1.join();
+	auto num = pms.get_future().get();
+	std::cout << num << '\n';
+
+	std::promise<int> pms2;
+	pms2.set_value(99);
+	//值传递到其他线程中
+	//通过static_cast消除重载函数的歧义
+	std::thread t2(static_cast<void(*)(std::promise<int>&)>(testGetValueThread), std::ref(pms2));
+	t2.join();
+
+	std::thread t3(static_cast<void(*)(int)>(testGetValueThread), num);
+	t3.join();
+}
+int main() {
+	testPromise();
+	return 0;
+}
